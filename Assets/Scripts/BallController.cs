ab21dbb1e32203461995cfce0e38d384026473bb
Caducs <caduwleite@gmail.com>
2021-08-30 19:00:46 -0300

    Aula 30/08

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Rigidbody2D ballRb;
+    public float ballSpeedX = 6;
+    public float ballSpeedY = 1.2f;
     void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        ballRb.velocity = new Vector2(ballSpeedX, ballSpeedY);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        ballSpeedX *= -1;
+        ballSpeedY *= -1;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,30 +6,35 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb2D;
     public int playerID;
-
+    public int playerSpeed = 5;
 
     void Update()
-    {
+    {   
         switch (playerID)
         {
             case 1: // if (playerID == 0)
                 if (Input.GetKey(KeyCode.S))
                 {
+                    rb2D.velocity = new Vector2(0,-playerSpeed);
+
                     Debug.Log("Apertando S");
                 }
                 if (Input.GetKey(KeyCode.W))
                 {
+                    rb2D.velocity = new Vector2(0, playerSpeed);
                     Debug.Log("Apertando W");
                 }
                 break;
             case 2: //else if (player == 1)
-                if (Input.GetKey(KeyCode.O))
+                if (Input.GetKey(KeyCode.UpArrow))
                 {
+                    rb2D.velocity = new Vector2(0, playerSpeed);
                     Debug.Log("Apertando pra O");
                 }
 
-                if (Input.GetKey(KeyCode.L))
+                if (Input.GetKey(KeyCode.DownArrow))
                 {
+                    rb2D.velocity = new Vector2(0, -playerSpeed);
                     Debug.Log("Apertando pra l");
                 }
                 break;
