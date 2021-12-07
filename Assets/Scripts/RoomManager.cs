1c5e4c3a45de5615c1ecc2039f4dc45939829abd
Caducs <caduwleite@gmail.com>
2021-12-07 19:07:21 -0300

    Aula 07/12

--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using Photon.Pun;
 using Photon.Realtime;
+using TMPro;
 
 public class RoomManager : MonoBehaviourPunCallbacks
 {
-
+    public TextMeshProUGUI playerName;
+    public TextMeshProUGUI opponentName;
+    private void Start()
+    {
+        SetPlayerNames();
+    }
+    public void SetPlayerNames() 
+    {
+        if (PhotonNetwork.IsMasterClient)
+            playerName.text = PhotonNetwork.NickName;
+        else
+            opponentName.text = PhotonNetwork.NickName;
+    }
     public override void OnDisconnected(DisconnectCause cause)
     {
         base.OnDisconnected(cause);
